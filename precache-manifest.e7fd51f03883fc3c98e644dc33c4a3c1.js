323edbd3567cafe297d5559cfdbb0e6a2455dd8e
skreddy_koppula <skreddy.koppula@gmail.com>
2023-07-16 22:23:24 +0530

    Updates

---
+++ precache-manifest.e7fd51f03883fc3c98e644dc33c4a3c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aced37716b2f37babf13",
+    "url": "/TextUtils-React/static/css/main.309382f8.chunk.css"
+  },
+  {
+    "revision": "aced37716b2f37babf13",
+    "url": "/TextUtils-React/static/js/main.aced3771.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "d61969fd3e7dee6a90b795ea025dde4b",
+    "url": "/TextUtils-React/index.html"
+  }
+];
